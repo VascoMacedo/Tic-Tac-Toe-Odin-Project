6532a192d6c925ae97679fb2154bc027e92730d6
Vasco Macedo <saruga19@sapo.pt>
2025-07-01 01:28:21 +0100

    Added More JS Stuff To Play The Game On Console.Log()

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function DashBoard() {
-  this.board = [1, 1, 1, 
+  this.board = [1, 1, 0, 
                 2, 1, 2,
-                1, 1, 3];
+                1, 0, 3];
                 
-  this.changeBoard = function(index){ 
+  this.changeBoard = function(index, flowController){ 
     console.log(this.board[index]);
+    if(flowController.player1Turn == true){
+      this.board[index] = 1;
+    }else{
+      this.board[index] = 2;
+    }
+
+    flowController.player1Turn = !flowController.player1Turn;
+    //let testTransfer = this.player1Turn;
+    //this.player1Turn = !testTransfer;
   }                
 }
 
@@ -17,47 +26,47 @@ function FlowController() {
     // AND Change Players Turn(SHOLD NOT BE HERE BUT KEEP IT FOR LATER)
     if(dashBoard.board[0] == dashBoard.board[1] && dashBoard.board[0] == dashBoard.board[2]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("FIRST HORIZONTAL IF CASE TEST WORKED!!!");
+      console.log(`FIRST HORIZONTAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
     if(dashBoard.board[3] == dashBoard.board[4] && dashBoard.board[3] == dashBoard.board[5]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("2 HORIZONTAL IF CASE TEST WORKED!!!");
+      console.log(`2 HORIZONTAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
     if(dashBoard.board[6] == dashBoard.board[7] && dashBoard.board[6] == dashBoard.board[8]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("3 HORIZONTAL IF CASE TEST WORKED!!!");
+      console.log(`3 HORIZONTAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
 
     //Check Vertical Lines
     if(dashBoard.board[0] == dashBoard.board[3] && dashBoard.board[0] == dashBoard.board[6]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("FIRST VERTICAL IF CASE TEST WORKED!!!");
+      console.log(`FIRST VERTICAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
     if(dashBoard.board[1] == dashBoard.board[4] && dashBoard.board[1] == dashBoard.board[7]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("2 VERTICAL IF CASE TEST WORKED!!!");
+      console.log(`2 VERTICAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
     if(dashBoard.board[2] == dashBoard.board[5] && dashBoard.board[2] == dashBoard.board[8]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("3 VERTICAL IF CASE TEST WORKED!!!");
+      console.log(`3 VERTICAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
 
     //Check Diagonal Lines
     if(dashBoard.board[0] == dashBoard.board[4] && dashBoard.board[0] == dashBoard.board[8]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("FIRST DIAGONAL IF CASE TEST WORKED!!!");
+      console.log(`FIRST DIAGONAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
     if(dashBoard.board[2] == dashBoard.board[4] && dashBoard.board[2] == dashBoard.board[6]){
       this.stillGoing = false;
-      this.player1Turn = !this.player1Turn;
-      console.log("2 DIAGONAL IF CASE TEST WORKED!!!");
+      console.log(`2 DIAGONAL IF CASE TEST WORKED! Player1 won: ${!this.player1Turn}`);
+      //this.player1Turn = !this.player1Turn;      
     }
   }
 
@@ -89,14 +98,15 @@ flowController = new FlowController();
 
 console.log(dashBoard.board);
 
-dashBoard.changeBoard(2);
-dashBoard.changeBoard(3);
+//dashBoard.changeBoard(2, flowController);
+//dashBoard.changeBoard(3, flowController);
+
+//ALTERNATE UNIVERSE
+console.log(`Player 1 turn: ${flowController.player1Turn}`);
+dashBoard.changeBoard(8, flowController);
+console.log(`Player 1 turn: ${flowController.player1Turn}`);
+dashBoard.changeBoard(4, flowController);
+console.log(`Player 1 turn: ${flowController.player1Turn}`);
 
 flowController.checkWinConditions(dashBoard);
 
-let testBollean = true;
-console.log("Testing  Boolean 1:")
-console.log(testBollean);
-testBollean = !testBollean;
-console.log("Testing  Boolean 2 (sould be False):")
-console.log(testBollean);
